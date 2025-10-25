3e5b5bf0610ecfc7a5db25fe54f052cc97d1de5c
JustFr4nck <francyperry07@gmail.com>
2025-10-25 17:50:46 +0200

    navbar first settings

---
+++ FrontEnd/src/components/MyNavbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">FluxChat</a>
+  </div>
+  <div class="flex gap-2">
+    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            :src="default_img_profile" />
+        </div>
+      </div>
+      <ul
+        tabindex="-1"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge text-yellow-500">Soon</span>
+          </a>
+        </li>
+        <li><a>Settings
+          <span class="badge text-yellow-500">Soon</span>
+        </a></li>
+        <li><router-link to="/login">Logout</router-link></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<router-view/>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+
+
+
+const default_img_profile = ref("../../public/images/profile_default.jpg");
+
+</script>
+
+
--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import MyNavbar from '@/components/MyNavbar.vue';
+
+
+
 </script>
 
 <template>
   <div class="flex flex-col h-screen items-center">
-    <h1>HOMEPAGE</h1>
-    <button class="cursor-pointer border mt-20">LOGOUT</button>
+    <MyNavbar/>
+
+
   </div>
 </template>
